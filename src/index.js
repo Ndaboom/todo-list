e069bf51648b8ab1f83fe86e54ffe236ad835f95
ndaboom <ndabosam084@gmail.com>
2022-11-02 21:52:38 +0100

    Tasks rendered dynamically via js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,32 @@ let tasksArr = [
     description: "Note description",
     completed: false,
     index: 0
+  },
+  {
+    description: "Play COD",
+    completed: false,
+    index: 1
+  },
+  {
+    description: "Go through JS docs",
+    completed: false,
+    index: 2
   }
 ];
 
 function populateUI(currentTask) {
-
+  const task = document.createElement('div');
+  task.className = 'task';
+  task.innerHTML = `
+    <input type="checkbox" />
+    <p>${currentTask.description}</p>
+    <span class="material-symbols-outlined">
+        list
+    </span>
+  `;
+  tasksContainer.appendChild(task);
 }
+
+tasksArr.forEach((task) => {
+  populateUI(task);
+});
