440e9d37cfa6d14195e8bf85007007c64a2bed1c
ndaboom <ndabosam084@gmail.com>
2022-11-02 21:34:39 +0100

    Main UI set

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+const tasksContainer = document.querySelector('.tasks-list');
+
+let tasksArr = [
+  {
+    description: "Note description",
+    completed: false,
+    index: 0
+  }
+];
+
+function populateUI(currentTask) {
+
+}
