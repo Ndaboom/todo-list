aae550bb287c615c5cf698973f78d8c6e9553959
ndaboom <ndabosam084@gmail.com>
2022-11-04 11:33:06 +0100

    ESLint error fixed

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './style.css';
 import Tasks from './tasks';
 
 Tasks = new Tasks();
+Tasks.readTasks();
--- src/tasks.js
+++ src/tasks.js
@@ -1,43 +1,42 @@
 export default class Tasks {
-    constructor(index, description, completed) {
-        this.index = index;
-        this.description = description;
-        this.completed = completed;
+  constructor(index, description, completed) {
+    this.index = index;
+    this.description = description;
+    this.completed = completed;
 
-        // For tasks rendering
-        this.tasksArr = [
-        {
-            description: 'Note description',
-            completed: false,
-            index: 0,
-        },
-        {
-            description: 'Play COD',
-            completed: false,
-            index: 1,
-        },
-        {
-            description: 'Go through JS docs',
-            completed: false,
-            index: 2,
-        },
-        ];
+    // For tasks rendering
+    this.tasksArr = [
+      {
+        description: 'Note description',
+        completed: false,
+        index: 0,
+      },
+      {
+        description: 'Play COD',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'Go through JS docs',
+        completed: false,
+        index: 2,
+      },
+    ];
 
-        this.tasksContainer = document.querySelector('.tasks-list');
-        this.readTasks();
+    this.tasksContainer = document.querySelector('.tasks-list');
     }
 
     populateTaskUI(currentTask) {
-        const task = document.createElement('div');
-        task.className = 'task';
-        task.innerHTML = `
+      const task = document.createElement('div');
+      task.className = 'task';
+      task.innerHTML = `
             <input type="checkbox" />
             <p>${currentTask.description}</p>
             <span class="material-symbols-outlined">
                 list
             </span>
-        `;
-        this.tasksContainer.appendChild(task);
+      `;
+      this.tasksContainer.appendChild(task);
     }
 
     readTasks() {
